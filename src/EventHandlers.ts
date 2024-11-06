37ecc305acf4a8af4c54ffd38d992717e736f415
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-06 12:11:16 +0100

    griffy

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -317,7 +317,7 @@ Props.MintEvent.handler(async ({ event, context }) => {
 });
 
 Griffy.MintEvent.handler(async ({ event, context }) => {
-  const collectionAddr = "0x3d199971f07f958a17a4f666a45a6619e4eebc5247efc911e723dbabd51e572f"
+  const collectionAddr = "0x0c10a1c5ef62b346a27a16cc4c270f5e74c1c94a7f8233fcf0223716b6c9f326"
   const receipent = event.params.recipient.payload.bits
   const token_id = event.params.token_id
   const collection = await context.Collection.get(collectionAddr);
