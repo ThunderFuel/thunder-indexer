ca5b18562998202b18b8d51b838296d76527ddb9
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-30 11:07:46 +0100

    add fuel dudes

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -10,6 +10,7 @@ import {
   Griffy,
   FuelPumps,
   Sangoro,
+  FuelDudes,
   eventLog,
   fuelTransferParams,
   handlerContext
@@ -370,6 +371,30 @@ FuelPumps.MintEvent.handler(async ({ event, context }) => {
 
 Sangoro.MintEvent.handler(async ({ event, context }) => {
   const collectionAddr = "0xb03ec5c6eeaf6d09ed6755e21dff896234c8f509b813f3ff17ef14a436fa8462"
+  const receipent = event.params.recipient.payload.bits
+  const token_id = event.params.token_id
+  const collection = await context.Collection.get(collectionAddr);
+  const nft_owner = `${token_id}=>${receipent}`
+
+  if (collection) {
+    const currentNftOwners = collection.owners
+    currentNftOwners.push(nft_owner)
+    context.Collection.set({
+      id: collectionAddr,
+      total_supply: token_id,
+      owners: currentNftOwners
+    })
+  } else {
+    context.Collection.set({
+      id: collectionAddr,
+      total_supply: token_id,
+      owners: [nft_owner]
+    })
+  }
+});
+
+FuelDudes.MintEvent.handler(async ({ event, context }) => {
+  const collectionAddr = "0x65aa85875bf92fb5b487ade154f88507d74b233ef901b4a172f4616b527a4784"
   const receipent = event.params.recipient.payload.bits
   const token_id = event.params.token_id
   const collection = await context.Collection.get(collectionAddr);
